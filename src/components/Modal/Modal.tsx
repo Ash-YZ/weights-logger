16a3c7947695a8fcada8691f9519a872932673e0
Ash_Y <ash.yusufzai@gamesglobal.com>
2022-02-07 14:59:13 +0000

    styling

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -25,7 +25,7 @@ const Modal: React.FC<Props> = ({
   return (
     <>
       <div className="fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full" />
-      <div className="flex justify-center h-full">
+      <div className="flex justify-center h-full z-50">
         <div className="absolute top-[20px] mx-auto p-5 border w-96 shadow-lg rounded-md bg-gray-500">
           <div className="mt-3 text-center">
             <h3 className="text-lg text-white">{title}</h3>
